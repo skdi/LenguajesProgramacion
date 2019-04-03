cdb086fb5176d551e94c347e385ba29bcf674304
Skdi <jddhghf@gmail.com>
2019-04-03 10:23:49 -0500

    Update suma.s

--- suma.s
+++ suma.s
@@ -6,15 +6,16 @@ main:
 	li $v0,5 #code 5 == read integer
 	syscall #invoke the operating system
 
-	li $v1,5 #leer entero
-	syscall
-
 	move $a0, $v0 #movemos el contenido de v0 a a0
-	move $a1, $v1 
 
-	add v0, a0, a1 #sumamos a0 en a1 y lo movemos a v0
+	li $v0,5 #leer entero
+	syscall
+	move $a1, $v0 
+
+	Add $a3, $a0, $a1 #sumamos a0 en a1 y lo movemos a v0
 
 	li $v0,1 #imprimimos v0
+	move $a0 ,$a3
 	syscall
 
 	jr $ra #fin del programa
