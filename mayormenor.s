1ec110f6268c4920ac621bba68bf48d9cd826b0d
Skdi <jddhghf@gmail.com>
2019-04-03 19:49:42 -0500

    Update mayormenor.s

--- mayormenor.s
+++ mayormenor.s
@@ -1,23 +1,19 @@
 
 .data 
-mayor_string: .asciiz "\nMayor que 10\n"
-menor_string: .asciiz "\nMenor que 10\n"
+mayor_str: .asciiz "\nMayor\n"
+menor_str: .asciiz "\nMenor\n"
 	.text 
 main: # Start of code section
 	li $v0,5
 	syscall
-
 	move $a1,$v0
-
 	bge $a1,10,dir
-	
 	li $v0,4
-	la $a0,menor_string
+	la $a0,menor_str
 	syscall
 	jr $ra
 
 dir:li $v0,4
-	la $a0,mayor_string
+	la $a0,mayor_str
 	syscall
-
 	jr $ra
